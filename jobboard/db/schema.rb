0463213c0a7145e1fea93e3de6eb8e135c82cfc9
Nicolas <nicolas.adam8@gmail.com>
2015-05-06 11:15:40 +0200

    modifs random ?(commit avant pull)

--- jobboard/db/schema.rb
+++ jobboard/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506074942) do
+ActiveRecord::Schema.define(version: 20150506075625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "offer_id"
+    t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
