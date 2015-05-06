ee6f2161e4d59f270af663003398c1c0c1d3d1e2
Vincent <vincent.stebe@gmail.com>
2015-05-06 10:32:40 +0200

    correction route

--- jobboard/config/routes.rb
+++ jobboard/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :applications
   resources :offers
   resources :companies
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
