81364c6b725a864170ca6cfc7e0025f6cbcd98ad
Vincent <vincent.stebe@gmail.com>
2015-05-07 09:52:08 +0200

    seed correct

--- jobboard/db/seeds.rb
+++ jobboard/db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 company = Company.create(name: 'Euro Information', mail: 'test@e-i.fr', phone: '01 23 45 67 89', address: 'Strasbourg', description: 'Filiale Crédit Mutuel !')
-User.create(user_type: 'admin', surname: 'De la Grandière', first_name: 'Richard',
- mail: 'richard.delagrandiere@e-i.fr', promo: 0, company: company, created_at: DateTime.new(2009,9,14,8),
- updated_at: DateTime.new(2009,9,14,8), email: 'plop', encrypted_password: 'plopplop', sign_in_count: 0)
+user =User.create!(user_type: 'admin', surname: 'De la Grandière', first_name: 'Richard',
+ mail: 'richard.delagrandiere@e-i.fr', promo: 0, company_id: 1, created_at: DateTime.new(2009,9,14,8),
+ updated_at: DateTime.new(2009,9,14,8), email: 'plop@bbb.com', password: 'plopplop',password_confirmation: 'plopplop', sign_in_count: 0)
 #Application:create(user_id)
