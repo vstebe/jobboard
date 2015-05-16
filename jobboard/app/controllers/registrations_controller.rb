c320eb3553832940dd5780586fd25e6707d2c051
Nicolas <nicolas.adam8@gmail.com>
2015-05-16 15:29:12 +0200

    ajout des vues sign_up et edit pour les utilisateurs

--- jobboard/app/controllers/registrations_controller.rb
+++ jobboard/app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :surname, :email, :password, :password_confirmation, :company_id, :promo)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :surname, :email, :password, :password_confirmation, :current_password, :company_id, :promo)
   end
 end
--- jobboard/app/models/user.rb
+++ jobboard/app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+         validates_presence_of :surname
+         validates_uniqueness_of :surname
+         validates_presence_of :first_name
+         validates_uniqueness_of :first_name
+
          belongs_to :company
          has_many :offers
          has_many :applications
