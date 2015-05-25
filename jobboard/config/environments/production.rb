04cd5febed19d49ea936f6e959f37c30a50a37f3
Vincent <vincent.stebe@gmail.com>
2015-05-25 15:29:55 +0200

    change url prod

--- jobboard/config/environments/production.rb
+++ jobboard/config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'https://ensiie-jobboard.scalingo.io/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "127.0.0.1", :port => 1025}
-  config.action_controller.asset_host = "http://localhost:3000"
-  config.action_mailer.asset_host = "http://localhost:3000"
+  config.action_controller.asset_host = "https://ensiie-jobboard.scalingo.io/"
+  config.action_mailer.asset_host = "https://ensiie-jobboard.scalingo.io/"
 
   config.action_mailer.smtp_settings = {
         :address              => "smtp.gmail.com",
